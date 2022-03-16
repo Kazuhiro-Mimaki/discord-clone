378c6322d0b6915b8157fd86dd9c65b7c7b74ec7
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2022-03-16 22:58:35 +0900

    Set router

--- discord-backend/src/main.py
+++ discord-backend/src/main.py
@@ -6,3 +6,18 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/api/auth", tags=["auth"])
+async def auth():
+    return {"auth": "call auth"}
+
+
+@app.post("/api/auth/register", tags=["register"])
+async def auth():
+    return {"register": "register"}
+
+
+@app.post("/api/auth/login", tags=["login"])
+async def auth():
+    return {"login": "login"}
